502cb52daf686d1c4f88ce7211764131ff242e0b
thanyi <yiceyuan@126.com>
2024-04-28 14:47:27 +0800

    change

--- hw01/hw01.py
+++ hw01/hw01.py
@@ -96,3 +96,16 @@ def hailstone(n):
     1
     """
     "*** YOUR CODE HERE ***"
+    num = 1
+
+    while 1:
+        if n == 1:
+            print(n)
+            return num
+        
+        print(n)
+        if n % 2 == 0 :
+            n = n//2 
+        else:
+            n = n*3 + 1
+        num += 1 
